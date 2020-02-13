df235f906d1f8d2b4c42bdadad5973fc3c665e11
AsociTon <asociton@outlook.com>
2020-02-13 18:49:29 +0530

    Fixed delete button.
    Now we can delete any item from the todo list as we wish to.
    TODO: Fix the save button.
    	  Desired behaviour: The save button should -
    	  1) write the new item in file
    	  2) close the newItem tab
    	  3) read all the items(i.e. refresh the list)

--- todo-app/one.py
+++ todo-app/one.py
@@ -1,5 +1,8 @@
 from tkinter import Tk, Label, Button, Entry, Text
 
+# Location of file to store the todo item list.
+FILE_PATH = 'myTodoList.txt'
+
 class todo:
     def __init__(self):
         self.newTab = Tk()
@@ -10,47 +13,57 @@ class todo:
         self.newTab.mainloop()
 
     def saveItem(self, task):
-        file = open('myTodo.txt', 'a')
-        file.write(task+"#*\n\n")
+        file = open(FILE_PATH, 'a')
+        file.write(task+"#*#")
         file.close()
+        self.newTab.destroy()
 
 class main:
     def __init__(self):
         self.mainWindow = Tk()
         self.items = []
         Button(self.mainWindow, text="+", command=self.addItem).grid(row=0,column=0)
+        
+        file = open(FILE_PATH, 'a')
+        file.close()
+
         self.readItems()
         self.mainWindow.mainloop()
 
     def deleteItem(self, n):
         print("n",n)
-        del self.items[n]
+        self.items[n][0].grid_forget()
+        self.items[n][1].grid_forget()
+        self.items[n] = []
         del self.fileItems[n]
         print("after deletion", self.items)
         for i in self.fileItems:
-            text = i+"#*\n\n"
-        file = open('myTodo.txt', 'w')
+            text = i+"#*#"
+        file = open(FILE_PATH, 'w')
         file.write(text)
         file.close()
         self.readItems()    
     
-    def refreshItems(self):
-        print("refreshit")
-
     def readItems(self):
-        file = open('myTodo.txt', 'r')
+        file = open(FILE_PATH, 'r')
         self.fileItems = file.read()
         file.close()
-        self.fileItems = self.fileItems.split("#*\n\n")
+        print(self.fileItems)
+        self.fileItems = self.fileItems.split("#*#")
         print("after split",self.fileItems)
         p=0
         for i in self.fileItems:
-            self.items.append([Label(self.mainWindow, text=i).grid(row=p+1, column=0), Button(self.mainWindow, text="X", command=lambda p=p: self.deleteItem(p))])
-            self.items[p][1].grid(row=p+1, column=1)
-            p+=1
+            if i != '':
+                self.items.append([Label(self.mainWindow, text=i), Button(self.mainWindow, text="X", command=lambda p=p: self.deleteItem(p))])
+                self.items[p][0].grid(row=p+1, column=0)
+                self.items[p][1].grid(row=p+1, column=1)
+                p+=1
 
     def addItem(self):
-        test = todo()
+        newItem = todo()
+        print("destroy")
+        newItem.newTab.destroy()
+        print("destroyed newTab")
         self.readItems()
 
     
